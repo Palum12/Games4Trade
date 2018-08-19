fd3c7ed427c6e30000e4797ac2511c631df43b0c
Maksymilian <paluszekmaksymilian@gmail.com>
2018-08-19 13:07:49 +0200

    simple navbar

---
+++ Client/Games4Trade/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light" id="mainNavbar">
+            <router-link class="navbar-brand" to="/"><a>Games4Trade</a></router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Test</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Zaloguj</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+    #mainNavbar {
+        background-color: #26bba6;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+    #mainNavbar a, .navbar {
+        color: white;
+    }
+</style>
---
+++ Client/Games4Trade/src/components/user/SignUp.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <p>Hello</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'signup'
+}
+</script>
+
+<style scoped>
+
+</style>
