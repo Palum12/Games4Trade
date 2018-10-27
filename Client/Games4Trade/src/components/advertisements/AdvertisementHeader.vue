e49ee4573abab4205ee400ea8d6e9c088c8752a5
Maksymilian <paluszekmaksymilian@gmail.com>
2018-10-27 16:48:16 +0200

    advertisement list and header

---
+++ Client/Games4Trade/src/components/advertisements/AdvertisementHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>{{advertisement.title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AdvertisementHeader',
+  props: ['advertisement']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Client/Games4Trade/src/components/advertisements/AdvertisementList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <p>No to jest lista</p>
+      <div class="inner" v-for="advertisement in advertisementList" :key="advertisement.id">
+          <advertisement-header :advertisement="advertisement"></advertisement-header>
+      </div>
+  </div>
+</template>
+
+<script>
+import AdvertisementHeader from './AdvertisementHeader'
+export default {
+  name: 'AdvertisementList',
+  components: {
+    AdvertisementHeader
+  },
+  props: ['advertisementList'],
+  methods: {
+    scrollEnded () {
+      var sh = document.getElementById('inner').scrollHeight
+      var st = document.getElementById('inner').scrollTop
+      var oh = document.getElementById('inner').offsetHeight
+      if (sh - st - oh + 1 < 2) {
+        this.$emit('scroll-ended')
+      }
+    }
+  },
+  mounted () {
+    document.getElementById('inner').addEventListener('scroll', this.scrollEnded)
+  }
+}
+</script>
+
+<style scoped>
+    #inner {
+        overflow: hidden;
+        overflow-y: auto;
+        -webkit-transform: translate3d(0, 0, 0);
+    }
+</style>
