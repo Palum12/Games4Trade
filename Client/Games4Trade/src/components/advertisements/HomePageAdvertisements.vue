eee01ae2630f302144c34180c5816bd1f398fded
Maksymilian <paluszekmaksymilian@gmail.com>
2018-10-28 21:02:07 +0100

    homepage but without pagination

--- Client/Games4Trade/src/components/advertisements/HomePageAdvertisements.vue
+++ Client/Games4Trade/src/components/advertisements/HomePageAdvertisements.vue
@@ -1,30 +1,72 @@
 <template>
     <div>
         <h1>Witaj w serwisie Games4Trade!</h1>
+        <div v-if="areRecommended">
+            <h3>Oto ogłoszenia polecane dla Ciebie !</h3>
+        </div>
+        <div v-else>
+            <h3>Oto najnowsze ogłoszenia !</h3>
+        </div>
+        <div class="scrollable-ads" >
+            <advertisement-list :advertisementList="advertisements"></advertisement-list>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import AdvertisementList from './AdvertisementList'
 export default {
   name: 'HomePageAdvertisements',
+  components: {
+    AdvertisementList
+  },
   data () {
     return {
       areRecommended: false,
+      userId: null,
       nextPage: 1,
-      isNextPage: true
+      pageSize: 10,
+      isNextPage: true,
+      advertisements: []
     }
   },
   mounted () {
+    let vm = this
     if (this.$store.getters.isAuthenticated) {
-      console.log('test')
+      this.$store.dispatch('getUserId')
+        .then(response => {
+          vm.userId = response.data
+        }).then(() => {
+          axios.get(`/users/${vm.userId}/advertisements/recommended?page=1`)
+            .then(response => {
+              if (response.data.length > 0) {
+                vm.areRecommended = true
+                vm.advertisements = response.data
+              } else {
+                vm.areRecommended = false
+                axios.get(`/advertisements/?page=${vm.nextPage}&size=${vm.pageSize}&desc=true`)
+                  .then(response => {
+                    vm.advertisements = response.data
+                  })
+              }
+            })
+        })
     } else {
-      this.areRecommended = true
+      vm.areRecommended = false
+      axios.get(`/advertisements/?page=${vm.nextPage}&size=${vm.pageSize}&desc=true`)
+        .then(response => {
+          vm.advertisements = response.data
+        })
     }
   }
 }
 </script>
 
 <style scoped>
-
+    .scrollable-ads {
+        min-height: 200px;
+        height: 72vh;
+        max-height: 100%;
+    }
 </style>
